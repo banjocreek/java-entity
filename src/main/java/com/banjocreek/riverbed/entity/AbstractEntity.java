a52403c8b5959ac2cf8f018d0e6db33bb20e371f
Greg Wiley <aztec.rex@jammm.com>
2015-01-26 17:40:19 -0800

    non-enum impl

---
+++ src/main/java/com/banjocreek/riverbed/entity/AbstractEntity.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (C) Greg Wiley
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License") under
+ * one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for information regarding copyright
+ * ownership. You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * Media Science Incorporated Confidential
+ *
+ * (C) Media Science Incorporated
+ *
+ * This file is subject to the terms and conditions defined in the
+ * file, 'NOTICE', which is part of this source code package.
+ */
+package com.banjocreek.riverbed.entity;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class AbstractEntity<K> {
+
+    protected final Map<K, Object> data;
+
+    protected AbstractEntity(final Map<K, Object> data) {
+
+        this.data = Collections.unmodifiableMap(new HashMap<>(data));
+
+    }
+
+    @Override
+    public final boolean equals(final Object obj) {
+
+        if (this == obj) {
+            return true;
+        }
+
+        if (this.getClass().isInstance(obj)) {
+            return ((AbstractEntity<?>) obj).data.equals(this.data);
+        }
+
+        return false;
+
+    }
+
+    @Override
+    public final int hashCode() {
+        return this.data.hashCode();
+    }
+
+    @Override
+    public final String toString() {
+        return this.data.toString();
+    }
+
+}
---
+++ src/main/java/com/banjocreek/riverbed/entity/AbstractMutableEntityBuilder.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (C) Greg Wiley
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License") under
+ * one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for information regarding copyright
+ * ownership. You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * Media Science Incorporated Confidential
+ *
+ * (C) Media Science Incorporated
+ *
+ * This file is subject to the terms and conditions defined in the
+ * file, 'NOTICE', which is part of this source code package.
+ */
+package com.banjocreek.riverbed.entity;
+
+import java.util.Map;
+import java.util.function.Function;
+
+import com.banjocreek.riverbed.builder.map.AbstractMutableMapBuilder;
+
+public abstract class AbstractMutableEntityBuilder<P, K, ENT extends AbstractEntity<K>, Z extends EntityBuilder<ENT, Z>>
+        extends AbstractMutableMapBuilder<K, Object, P> implements
+        EntityBuilder<ENT, Z> {
+
+    protected AbstractMutableEntityBuilder(
+            final Function<Map<K, Object>, P> constructor) {
+        super(constructor);
+    }
+
+    @Override
+    public final Z withDefaults(final ENT mdefs) {
+        defaults(mdefs.data);
+        @SuppressWarnings("unchecked")
+        final Z z = (Z) this;
+        return z;
+    }
+
+    @Override
+    public final Z withValues(final ENT mvals) {
+        defaults(mvals.data);
+        @SuppressWarnings("unchecked")
+        final Z z = (Z) this;
+        return z;
+    }
+
+}
